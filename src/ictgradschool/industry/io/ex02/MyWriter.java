656f35d04ab162629cee3bc5331a260f93d9af9a
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-27 12:19:34 +1300

    did ex02 1.

--- src/ictgradschool/industry/io/ex02/MyWriter.java
+++ src/ictgradschool/industry/io/ex02/MyWriter.java
@@ -2,6 +2,8 @@ package ictgradschool.industry.io.ex02;
 
 import ictgradschool.Keyboard;
 
+import java.io.*;
+
 /**
  * A simple program which should allow the user to type any number of text lines. The program will then
  * write them out to a file.
@@ -13,22 +15,27 @@ public class MyWriter {
         System.out.print("Enter a file name: ");
         String fileName = Keyboard.readInput();
 
-        // TODO Open a file for writing, using a PrintWriter.
+        // TODOne Open a file for writing, using a PrintWriter.
+        try (PrintWriter writer = new PrintWriter(new FileOutputStream(new File(fileName+".txt"),true))) {
 
-        while (true) {
+            while (true) {
 
-            System.out.print("Type a line of text, or just press ENTER to quit: ");
-            String text = Keyboard.readInput();
+                System.out.print("Type a line of text, or just press ENTER to quit: ");
+                String text = Keyboard.readInput();
 
-            if (text.isEmpty()) {
-                break;
+                if (text.isEmpty()) {
+                    break;
+                }
+
+                // TODOne Write the user's line of text to a file.
+                writer.println(text);
             }
 
-            // TODO Write the user's line of text to a file.
+            System.out.println("Done!");
+
+        } catch (IOException e){
+            System.out.println("oops, err "+e);
         }
-
-        System.out.println("Done!");
-
     }
 
     public static void main(String[] args) {
