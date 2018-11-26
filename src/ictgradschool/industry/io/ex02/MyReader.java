6e47e76c762921f9e855cff47db992fd6e318982
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-27 12:25:37 +1300

    did ex02 2.

--- src/ictgradschool/industry/io/ex02/MyReader.java
+++ src/ictgradschool/industry/io/ex02/MyReader.java
@@ -1,11 +1,32 @@
 package ictgradschool.industry.io.ex02;
 
+import ictgradschool.Keyboard;
+
+import java.io.*;
+
 public class MyReader {
 
     public void start() {
 
         // TODO Prompt the user for a file name, then read and print out all the text in that file.
         // TODO Use a BufferedReader.
+
+        System.out.print("Enter a file name: ");
+        String fileName = Keyboard.readInput();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File(fileName+".txt")))){
+
+            String line = null;
+            while ((line= reader.readLine())!=null){
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e){
+            System.out.println("FnF err "+e);
+        } catch (IOException e){
+            System.out.println("IO err "+e);
+        }
+
     }
 
     public static void main(String[] args) {
