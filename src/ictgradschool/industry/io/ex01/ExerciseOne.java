127fd2fea8cbd8085d6a67df2e6aaa9284461802
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-11-27 12:01:56 +1300

    did Ex01 2&3. but 3 gives two fewer total char

--- src/ictgradschool/industry/io/ex01/ExerciseOne.java
+++ src/ictgradschool/industry/io/ex01/ExerciseOne.java
@@ -1,5 +1,7 @@
 package ictgradschool.industry.io.ex01;
 
+import java.io.*;
+
 public class ExerciseOne {
 
     public void start() {
@@ -15,8 +17,25 @@ public class ExerciseOne {
         int numE = 0;
         int total = 0;
 
-        // TODO Read input2.txt and print the total number of characters, and the number of e and E characters.
-        // TODO Use a FileReader.
+        // TODOne Read input2.txt and print the total number of characters, and the number of e and E characters.
+        // TODOne Use a FileReader.
+        int num =0;
+
+        try(FileReader fR = new FileReader("input2.txt")) {
+
+            while((num=fR.read())!=-1){
+                if((char)num=='E'||(char)num=='e'){
+                    numE++;
+                    total++;
+                } else {
+                    total++;
+                }
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("FnF exc. "+e);
+        } catch (IOException e){
+            System.out.println("IO exc. "+e);
+        }
 
         System.out.println("Number of e/E's: " + numE + " out of " + total);
     }
@@ -28,6 +47,24 @@ public class ExerciseOne {
 
         // TODO Read input2.txt and print the total number of characters, and the number of e and E characters.
         // TODO Use a BufferedReader.
+        File theFile = new File("input2.txt");
+        try(BufferedReader bF= new BufferedReader(new FileReader(theFile))){
+            String line = null;
+            while((line=bF.readLine())!=null){
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i)=='e'||line.charAt(i)=='E'){
+                        numE++;
+                        total++;
+                    } else {
+                        total++;
+                    }
+                }
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("FnF err: "+e);
+        } catch (IOException e){
+            System.out.println("IO err: "+e);
+        }
 
         System.out.println("Number of e/E's: " + numE + " out of " + total);
     }
