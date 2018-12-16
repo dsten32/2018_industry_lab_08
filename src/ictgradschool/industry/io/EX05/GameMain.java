122edc17679ba096825b24b2509f5674807e93e0
Darcy Cowan <dwc1@students.waikato.ac.nz>
2018-12-17 11:47:28 +1300

    got game working, I think. Now to get read in.

--- src/ictgradschool/industry/io/EX05/GameMain.java
+++ src/ictgradschool/industry/io/EX05/GameMain.java
@@ -3,65 +3,119 @@ package ictgradschool.industry.io.EX05;
 import java.util.Scanner;
 
 public class GameMain {
-    String playerCode, computerCode;
-
+    String playerGuess,playerCode, computerGuess,computerCode,bullPlural,cowPlural;
+    int cow,bull,num_guesses=0;
+    Scanner sc = new Scanner(System.in);
 
     private void start() {
-        Scanner sc = new Scanner(System.in);
+
         playerCode="";
         // generate computer code
 
         computerCode=compCode();
 
-        while (playerCode.length()<=3||playerCode.length()>=5) {
-            try {
-                System.out.println("Please enter your secret code:");
-                playerCode = sc.next();
-                checkEntry(playerCode);
-            } catch (NumberFormatException e) {
-                System.out.println("nope, not a num");
-            } catch (InvalidEntryException e){
-                System.out.println("ugh, wrong number of digits..");
+        System.out.println("Please enter your secret code:");
+
+
+        playerCode = playerEntry();
+
+
+
+
+
+        while (num_guesses<=7) {
+            System.out.print("--- \nYou Guess:");
+
+
+            playerGuess=playerEntry();
+
+//            playerGuess=sc.next();
+            test(playerGuess, computerCode);
+
+            System.out.println("Result: " + bull + " " + bullPlural + " and " + cow + " " + cowPlural);
+            System.out.print(computerCode);
+
+            if(checkWin()){
+                System.out.print("You win! :)");
+                return;
             }
+            System.out.println();
+            computerGuess=compCode();
+            test(computerGuess,playerCode);
+            System.out.println("Computer Guess: "+computerGuess);
+            System.out.println("Result: " + bull + " " + bullPlural + " and " + cow + " " + cowPlural);
+
+            if(checkWin()){
+                System.out.print("Computer wins! :P");
+                return;
+            }
+
+            num_guesses++;
         }
 
-        test(playerCode,computerCode);
-
-//        test(playerCode,computerCode,new CompareGuess());
-
-        System.out.println("---");
-        System.out.println("You Guess: "+playerCode);
-
-
-//        System.out.println("Me Guess: "+computerCode);
-
-        // add in testing for comparing player guess to computer code
-
-
-
-        System.out.println("Result: "+"haven't figured this part out yet");
-
+        System.out.print("\nYou're out of guesses, bye.");
 
 
     }
 
     private void test(String guess, String code){
-        int cow = 0,bull=0;
+        cow=0;
+        bull=0;
+        String temp=code;
         for(int i=0;i<guess.length();i++) {
-            for (int j = 0; j < code.length(); j++) {
-                if (guess.charAt(i)==code.charAt(j)){
-
-                }
+            if(guess.charAt(i)==temp.charAt(i)){
+                bull++;
+            }
+            if (temp.contains(Character.toString(guess.charAt(i)))){
+                cow++;
+                temp=temp.replace(Character.toString(guess.charAt(i)),"^");
             }
         }
 
+        cow-=bull;
+
+        if(bull==1){
+            bullPlural="Bull";
+        } else {
+            bullPlural="Bulls";
+        }
+        if(cow==1){
+            cowPlural="cow";
+        } else {
+            cowPlural="cows";
+        }
     }
+    //            for (int j = 0; j < code.length(); j++) {
+//                if (guess.charAt(i)==code.charAt(j)){
+//
+//                }
+//            }
+    private String playerEntry(){
+        String entry="";
+        while (entry.length()<=3||entry.length()>=5) {
+            try {
+                entry = sc.next();
+                Integer.parseInt(String.valueOf(entry));
+                checkEntry(entry);
+            } catch (NumberFormatException e) {
+                System.out.println("nope, not a num");
+            } catch (InvalidEntryException e) {
+                System.out.println("ugh, wrong number of digits..");
+            }
+        }
+        return entry;
+    }
+
 
     private void checkEntry(String guess) throws InvalidEntryException{
 
         if (guess.length()<4 || guess.length()>4){
             throw new InvalidEntryException();
         }
+    }
+
+    private boolean checkWin(){
+        return (bull==4);
     }
 
     private String compCode(){
